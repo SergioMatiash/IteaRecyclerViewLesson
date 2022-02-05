52764f006aec1d1a7b667c2dbeee6f8150b031c0
Serhii Antoniuk <sergiomatiash0@gmail.com>
2022-02-05 13:39:49 +0200

    Second commit- compiled --- view problem --- v.1.0.1

---
+++ app/src/main/java/com/example/itearecyclerviewlesson/view/ContactItem.java
@@ -0,0 +1,16 @@
+package com.example.itearecyclerviewlesson.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.itearecyclerviewlesson.R;
+
+public class ContactItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_item);
+    }
+}
